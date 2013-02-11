a9a944bc296091062b5dc05c0c3d1360d2b809b1
Brad Anderson <brad@sankatygroup.com>
2013-02-10 23:57:37 -0500

    formatting

--- src/main/java/com/mapr/franz/server/CatcherServiceImpl.java
+++ src/main/java/com/mapr/franz/server/CatcherServiceImpl.java
@@ -1,91 +1,95 @@
 package com.mapr.franz.server;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import com.google.protobuf.RpcController;
 import com.google.protobuf.ServiceException;
 import com.mapr.franz.catcher.Client;
 import com.mapr.franz.catcher.wire.Catcher;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 /**
  * The kinda broken server.
  */
 public class CatcherServiceImpl implements
-		Catcher.CatcherService.BlockingInterface {
-	private final long serverId;
+        Catcher.CatcherService.BlockingInterface {
 
-	private ClusterState state;
+    private final long serverId;
 
-	// TODO implement some sort of statistics that records (a) number of
-	// clients, (b) transactions per topic, (c) bytes per topic
+    private ClusterState state;
 
-	public CatcherServiceImpl(long serverId, ClusterState state) {
-		this.serverId = serverId;
-		this.state = state;
-	}
+    // TODO implement some sort of statistics that records (a) number of
+    // clients, (b) transactions per topic, (c) bytes per topic
 
-	@Override
-	public Catcher.HelloResponse hello(RpcController controller,
-			Catcher.Hello request) throws ServiceException {
-		Catcher.HelloResponse.Builder r = Catcher.HelloResponse.newBuilder()
-				.setServerId(serverId);
+    public CatcherServiceImpl(long serverId, ClusterState state) {
+        this.serverId = serverId;
+        this.state = state;
+    }
 
-		for (Client.HostPort address : state.getLocalInfo().getAddresses()) {
-			Catcher.Host.Builder host = r.addHostBuilder();
-			host.setHostName(address.getHost());
-			host.setPort(address.getPort());
-			host.build();
-		}
+    @Override
+    public Catcher.HelloResponse hello(RpcController controller,
+            Catcher.Hello request) throws ServiceException {
+        Catcher.HelloResponse.Builder r = Catcher.HelloResponse.newBuilder()
+                .setServerId(serverId);
 
-		for (Catcher.Server server : state.getCluster()) {
-			Catcher.Server.Builder c = r.addClusterBuilder();
-			c.mergeFrom(server);
-			c.build();
-		}
-		return r.build();
-	}
+        for (Client.HostPort address : state.getLocalInfo().getAddresses()) {
+            Catcher.Host.Builder host = r.addHostBuilder();
+            host.setHostName(address.getHost());
+            host.setPort(address.getPort());
+            host.build();
+        }
 
-	@Override
-	public Catcher.LogMessageResponse log(RpcController controller,
-			Catcher.LogMessage request) throws ServiceException {
-		String topic = request.getTopic();
+        for (Catcher.Server server : state.getCluster()) {
+            Catcher.Server.Builder c = r.addClusterBuilder();
+            c.mergeFrom(server);
+            c.build();
+        }
+        return r.build();
+    }
 
-		Catcher.LogMessageResponse.Builder r = Catcher.LogMessageResponse
-				.newBuilder().setServerId(serverId).setSuccessful(true);
+    @Override
+    public Catcher.LogMessageResponse log(RpcController controller,
+            Catcher.LogMessage request) throws ServiceException {
 
-		ClusterState.Target directTo = state.directTo(topic);
+        String topic = request.getTopic();
 
-		if (directTo.getStatus() == ClusterState.Status.LIVE) {
-			if (directTo.isRedirect()) {
-				// TODO actually forward request to other server
-				r.getRedirectBuilder().setTopic(topic)
-						.setServer(directTo.getServer()).build();
-			}
-		} else {
-			ClusterStateException e = new ClusterStateException(
-					"Can't handle request ... can't see rest of cluster");
-			StringWriter s = new StringWriter();
+        Catcher.LogMessageResponse.Builder r = Catcher.LogMessageResponse
+                .newBuilder().setServerId(serverId).setSuccessful(true);
 
-			PrintWriter pw = new PrintWriter(s);
-			e.printStackTrace(pw);
-			pw.close();
+        ClusterState.Target directTo = state.directTo(topic);
 
-			r.setSuccessful(false).setBackTrace(s.toString());
-		}
+        if (directTo.getStatus() == ClusterState.Status.LIVE) {
+            if (directTo.isRedirect()) {
+                // TODO actually forward request to other server
+                r.getRedirectBuilder().setTopic(topic)
+                        .setServer(directTo.getServer()).build();
+            }
+        } else {
+            ClusterStateException e = new ClusterStateException(
+                    "Can't handle request ... can't see rest of cluster");
+            StringWriter s = new StringWriter();
 
-		return r.build();
-	}
+            PrintWriter pw = new PrintWriter(s);
+            e.printStackTrace(pw);
+            pw.close();
 
-	@Override
-	public Catcher.CloseResponse close(RpcController controller,
-			Catcher.Close request) throws ServiceException {
-		return Catcher.CloseResponse.newBuilder().build();
-	}
+            r.setSuccessful(false).setBackTrace(s.toString());
+        }
 
-	private class ClusterStateException extends Exception {
-		public ClusterStateException(String msg) {
-			super(msg);
-		}
-	}
+        return r.build();
+    }
+
+    @Override
+    public Catcher.CloseResponse close(RpcController controller,
+            Catcher.Close request) throws ServiceException {
+        return Catcher.CloseResponse.newBuilder().build();
+    }
+
+    private class ClusterStateException extends Exception {
+        private static final long serialVersionUID = -1473457816312999824L;
+
+        public ClusterStateException(String msg) {
+            super(msg);
+        }
+    }
 }
